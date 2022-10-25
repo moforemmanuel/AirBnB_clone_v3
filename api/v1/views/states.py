06738ec131dce6c209c08e789468f83346ec04c8
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-25 15:37:21 +0100

    feat: add states router handler

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,25 +2,53 @@
 """states"""
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request, make_response
 from models import storage
+from models.state import State
 
 
-@app_views.route("/states", methods=['GET'])
+@app_views.route("/states", methods=['GET', 'POST'], strict_slashes=False)
 def states():
     raw_states = storage.all('State')
     states_objs = [state_obj.to_dict() for state_obj in raw_states.values()]
-    return jsonify(states_objs)
+
+    # get method
+    if request.method == 'GET':
+        return jsonify(states_objs)
+
+    # post method
+    if request.method == 'POST':
+        if request.headers['Content-Type'] != 'application/json':
+            abort(400, 'Not a JSON')
+        else:
+            body = request.get_json()
+            if body.get('name') is None:
+                abort(400, 'Missing name')
+            else:
+                print(body)
+                new_state = State(**body)
+                new_state.save()
+                return make_response(jsonify(new_state.to_dict()), 201)
 
 
-@app_views.route("/states/<state_id>", methods=['GET', 'DELETE', 'POST', 'PUT'])
+@app_views.route("/states/<state_id>", methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
 def state(state_id):
-    raw_states = storage.all('State')
-    state_obj = None
-    for key, value in raw_states.items():
-        if state_id == key.split('.')[1]:
-            state_obj = value.to_dict()
-            break
-    if state_obj is None:
+    state_instance = storage.get(State, state_id)
+    if state_instance is None:
         abort(404)
-    return jsonify(state_obj)
+    else:
+        if request.method == 'GET':
+            return jsonify(state_instance.to_dict())
+        if request.method == 'DELETE':
+            storage.delete(state_instance)
+            return make_response(jsonify({}), 200)
+        if request.method == 'PUT':
+            if request.headers['Content-Type'] != 'application/json':
+                abort(400, 'Not a JSON')
+            else:
+                body = request.get_json()
+                for key, value in body.items():
+                    if key not in ('id', 'created_at', 'updated_at'):
+                        setattr(state_instance, key, value)
+            return make_response(jsonify(state_instance.to_dict()), 200)
